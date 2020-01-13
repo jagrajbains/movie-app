13fc5ec6e7e32f21790e1a6eb508ded59757bd93
Abhishek M <abhishek.m@so.city>
2020-01-13 14:40:29 +0530

    fix tests

--- components/sideMenu.js
+++ components/sideMenu.js
@@ -1,6 +1,6 @@
 const SideMenu = (props) => (
     <>
-        <h1 className="my-4">{props.appName}!</h1>
+        <h1 className="my-4">{props.appName}</h1>
         <div className="list-group">
           <a href="#" className="list-group-item">Category 1</a>
           <a href="#" className="list-group-item">Category 2</a>
